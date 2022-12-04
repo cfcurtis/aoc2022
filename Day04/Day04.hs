e2d6db7b3f22af2c5a3c57245ab8453a19e166c5
Charlotte Curtis <ccurtis@mtroyal.ca>
2022-12-04 11:00:03 -0700

    Adding Day04 with some magic refactoring that I don't fully understand

---
+++ Day04/Day04.hs
@@ -0,0 +1,42 @@
+import System.IO ()
+import Data.List.Split (splitOn)
+
+-- Converts a string of the form "X-Y" into a list of corresponding integers
+toRange :: String -> [Int]
+toRange section = map read $ splitOn "-" section :: [Int]
+
+-- Takes a pair of ranges and checks whether they fully contain the other
+isContained :: [[Int]] -> Int
+isContained [a, b]
+    | head a >= head b && a !! 1 <= b !! 1 = 1
+    | head b >= head a && b !! 1 <= a !! 1 = 1
+    | otherwise = 0
+isContained _ = 0
+
+-- Takes a pair of ranges and checks whether there is any overlap
+anyOverlap :: [[Int]] -> Int
+anyOverlap [a, b]
+    | head a <= b !! 1 && a !! 1 >= head b = 1
+    | head b <= a !! 1 && b !! 1 >= head a = 1
+    | otherwise = 0
+anyOverlap _ = 0
+
+-- Apply either isContained or anyOverlap to the input
+checkPairs :: ([[Int]] -> Int) -> String -> Int
+checkPairs f contents = sum $ map ((f . map toRange) . splitOn ",") (lines contents)
+-- Version before refactoring hints below
+-- sum $ map f (map (map toRange) (map (splitOn ",") $ lines contents))
+
+-- How many ranges fully contain the other in a given pair?
+part1 :: String -> Int
+part1 = checkPairs isContained
+
+part2 :: String -> Int
+part2 = checkPairs anyOverlap
+
+main :: IO ()
+main = do
+    contents <- readFile inFile
+    print $ part1 contents
+    print $ part2 contents
+    where inFile = "input.txt"
