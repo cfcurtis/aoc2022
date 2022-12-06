75a5d68f7baa21e014885fd9122df26abf0bd20b
Charlotte Curtis <ccurtis@mtroyal.ca>
2022-12-05 21:11:46 -0700

    Adding solution to day 5, part 2

--- src/Day05.hs
+++ src/Day05.hs
@@ -21,7 +21,7 @@ readInstructions contents =
 modStack :: [Char] -> Int -> Int -> (Int, String) -> String
 modStack crate from to (index, stack)
     | null crate = stack -- Don't drop an empty crate
-    | index == from = drop 1 stack -- drop the crate from the stack if the current index is the from index
+    | index == from = drop (length crate) stack -- drop the crate(s) from the stack if the current index is the from index
     | index == to = crate ++ stack -- push the crate onto the stack if the current index is the to index
     | otherwise = stack -- otherwise, leave the stack unchanged
 
@@ -32,17 +32,25 @@ moveCrate crates (num:from:to:rest)
     | num == 1 = zipWith (curry (modStack crate from to)) [1..(length crates)] crates
     | from == to = crates
     | otherwise = moveCrate (moveCrate crates [1, from, to]) [num - 1, from, to]
-        where crate = trace (show crates ++ show [num, from, to]) take 1 $ crates !! (from - 1)
+        where crate = take 1 $ crates !! (from - 1)
 moveCrate _ _ = []
+
+-- move stacks of crates according to the instructions
+moveStacks :: [String] -> [Int] -> [String]
+moveStacks crates (num:from:to:rest)
+    | from == to = crates
+    | otherwise = zipWith (curry (modStack crate from to)) [1..(length crates)] crates
+        where crate = take num $ crates !! (from - 1)
+moveStacks _ _ = []
 
 -- Which crates are on top of the stacks?
 part1 :: String -> String
 part1 contents = map head $ foldl moveCrate (readCrates contents) (readInstructions contents)
 
--- Placeholder for part 2
-part2 :: String -> Int
-part2 contents = 0
+-- Which crates are on top of the stacks when multiples move at once?
+part2 :: String -> String
+part2 contents = map head $ foldl moveStacks (readCrates contents) (readInstructions contents)
 
 solve :: String -> IO ()
 solve contents = putStrLn "--- Day 05 ---" >>
-    putStrLn (part1 contents) >> print (part2 contents)
+    putStrLn (part1 contents) >> putStrLn (part2 contents)
